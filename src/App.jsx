5abcbb349083c3164a5509cfa0d9b3e20296a172
George-25 <georgemallory26@gmail.com>
2025-03-20 21:09:09 -0400

    added provider inside app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
-// import Parent from "./components/router/parent";
-import ParentManager from "./components/parent";
+// import React from "react";
+// // import Parent from "./components/router/parent";
+// import ParentManager from "./components/parent";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <ParentManager />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import ThemeToggle from "./ThemeToggle";
+
+const App = () => {
   return (
-    <>
-      <div>
-        <ParentManager />
-      </div>
-    </>
+    <ThemeProvider>
+      <ThemeToggle />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
