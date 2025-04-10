648abe122f3c6057fabf5e8a52c3d40d063e4cee
George-25 <georgemallory26@gmail.com>
2025-04-09 21:39:15 -0400

    added some codes in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,3 +47,26 @@
 // };
 
 // export default App;
+/////////////////////////////////
+import React from "react";
+import Table from "./components/Table";
+import EditModal from "./components/EditModal";
+import DeleteModal from "./components/DeleteModal";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Table />
+      </div>
+      <div>
+        <EditModal />
+      </div>
+      <div>
+        <DeleteModal />
+      </div>
+    </>
+  );
+};
+
+export default App;
