f5f2b17b25ec721c471b8ac391e0fcd199d5fae1
George-25 <georgemallory26@gmail.com>
2025-04-11 13:13:39 -0400

    added some codes in modal jsx

--- src/portal/Modal.jsx
+++ src/portal/Modal.jsx
@@ -1,6 +1,13 @@
 import ReactDom from "react-dom";
 
-const Modal = () => {
-  return ReactDom.createPortal();
+const Modal = ({ children }) => {
+  const modalRoot = document.getElementById("modal-root");
+
+  return ReactDom.createPortal(
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      {children}
+    </div>,
+    modalRoot
+  );
 };
 export default Modal;
