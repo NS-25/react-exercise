c0de835a9a3bdb8c02d3aa026b38908244dfcda5
George-25 <georgemallory26@gmail.com>
2025-04-11 13:14:54 -0400

    added modal jsx in editmodal

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -9,34 +9,36 @@ const EditModal = ({ row, onClose, onUpdate }) => {
     onUpdate({ ...row, ...form });
   };
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
-      <div className="bg-white p-6 rounded w-96 shadow-lg ">
-        <h2 className="text-lg font-semibold mb-4">Edit User</h2>
-        <input
-          className="border p-2 mb-3 w-full"
-          value={form.name}
-          onChange={(e) => setForm({ ...form, name: e.target.value })}
-          placeholder="Name"
-        />
-        <input
-          className="border p-2 mb-3 w-full"
-          value={form.email}
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-          placeholder="Email"
-        />
-        <div className="flex justify-end gap-2">
-          <button onClick={onClose} className="bg-gray-300 px-4 py-1 rounded">
-            Cancel
-          </button>
-          <button
-            onClick={handleSubmit}
-            className="bg-green-500 text-white px-4 py-1 rounded"
-          >
-            Update
-          </button>
+    <Modal>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+        <div className="bg-white p-6 rounded w-96 shadow-lg ">
+          <h2 className="text-lg font-semibold mb-4">Edit User</h2>
+          <input
+            className="border p-2 mb-3 w-full"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            placeholder="Name"
+          />
+          <input
+            className="border p-2 mb-3 w-full"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            placeholder="Email"
+          />
+          <div className="flex justify-end gap-2">
+            <button onClick={onClose} className="bg-gray-300 px-4 py-1 rounded">
+              Cancel
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="bg-green-500 text-white px-4 py-1 rounded"
+            >
+              Update
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 
