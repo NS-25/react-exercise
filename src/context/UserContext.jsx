b8f69396720c7d32af954112b486b4064dbdb2af
George-25 <georgemallory26@gmail.com>
2025-04-10 21:39:44 -0400

    use context done and ready to use

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,9 +4,12 @@ import { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({}) => {
+  const [data, setData] = useState([]);
   return (
     <UserContext.Provider value={{ data, setData }}>
       {Children}
     </UserContext.Provider>
   );
 };
+
+export const useUser = () => useContext(UserContext);
