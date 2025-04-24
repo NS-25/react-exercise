dd8e9d661866e630f2e39bfcc371df822658f88b
George-25 <georgemallory26@gmail.com>
2025-04-23 23:48:58 -0400

    added some code

--- src/components/useEffect/UseEffect.jsx
+++ src/components/useEffect/UseEffect.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
+// What is useEffect?
+
+// useEffect is a React Hook that lets you perform side effects in function components.
+
+// Think of side effects like:
+// 	•	Fetching data from an API
+// 	•	Updating the DOM manually
+// 	•	Setting up subscriptions
+// 	•	Setting a timer
+
+// Basic Syntax
+useEffect(() => {
+  // This code runs after the component is rendered
+}, []);
 
 function UseEffect() {
   return (
